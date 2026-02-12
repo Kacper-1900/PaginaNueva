2e541a40f69004b2c904852a658e6a17e3f5e567
kacper <kacperok201@gmail.com>
2026-02-12 23:18:09 +0000

    add changes to github

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,45 @@
-function Footer(){
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
     return (
-        <footer className="footer-main">
-            <p>© 2024 Mi Página de Fotos. Todos los derechos reservados.</p>
+        <footer className="footer">
+
+            <div className="footer-legal">
+                © 2026 PicturesKacper. Todos los derechos reservados.                
+                <a href="/privacy">Política de Privacidad y Cookies</a> |
+                <a href="/terms">Condiciones de Venta</a>
+            </div>
+
+            <div className="footer-social">
+
+                <div className="social-item">
+                    <spam>Instagram</spam>
+                    <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+                        <img src="/icons/instagram.svg" alt="Instagram" />
+                    </a>
+                </div>
+
+                <div className="social-item">
+                    <spam>Twitter</spam>
+                    <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
+                        <img src="/icons/twitter.svg" alt="Twitter" />
+                    </a>
+                </div>
+            </div>
+
+
+            <div className="footer-links">
+                <a href="https://github.com/26Saul/RealShoes/tree/develop" target="_blank" rel="noreferrer">
+                    GitHub
+                </a>
+                <a href="https://www.figma.com/file/tu-proyecto" target="_blank" rel="noreferrer">
+                    Figma
+                </a>
+                <a href="/">Inicio</a>
+            </div>
         </footer>
     );
 }
 export default Footer;
+
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,30 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
 function Header() {
-    const [isSideMenuVisible, setIsSideMenuVisible] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(!menuOpen);
 
-    const toggleSideMenu = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">
+          <img src="/RealShoes.jpg" alt="RealShoes Logo" />
+        </Link>
+      </div>
 
-        setIsSideMenuVisible(prev => !prev);
-    }
-    return (
-        <>
-            <div className="header-main">
-                <h1>Mi Página de Fotos</h1>
-                <img src="/public/logo.png" alt="Logo de la página" />
-            </div>
-            {
-                
-                isSideMenuVisible ?
+      <div className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </div>
 
-                    <div>
-                        <div className='header-side-menu'>
-                            <ul>
-                                <li>Inicio</li>
-                                <li>menu</li>
-                                <li>reclamaciones</li>
-                            </ul>
-                        </div>
-                    </div>
-                    :
-
-                    <></>
-            }
-        </>
-    );
+      <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
+        <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
+        <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
+        <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+      </nav>
+    </header>
+  );
 }
+
 export default Header;
